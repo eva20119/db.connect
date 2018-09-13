6ca701d9b040f7047e19702c84c5004020d590b6
henry <ah13441673@gmail.com>
2018-09-13 05:17:22 +0000

    add base_inform_configlet

---
+++ src/db/connect/browser/base_inform_configlet.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from db.connect import _
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from zope import schema
+from plone.z3cform import layout
+from z3c.form import form
+from plone.directives import form as Form
+
+
+class IInform(Form.Schema):
+
+    email = schema.TextLine(
+        title=_(u'email'),
+        required=False,
+    )
+    cellphone = schema.TextLine(
+        title=_(u'cellphone'),
+        required=False,
+    )
+    address = schema.TextLine(
+        title=_(u'address'),
+        required=False
+    )
+    fax = schema.TextLine(
+        title=_(u'fax'),
+        required=False
+    )
+
+class BasicInformControlPanelForm(RegistryEditForm):
+    form.extends(RegistryEditForm)
+    schema = IInform
+
+CustomControlPanelView = layout.wrap_form(BasicInformControlPanelForm, ControlPanelFormWrapper)
+CustomControlPanelView.label = _(u"Basic Inform")
